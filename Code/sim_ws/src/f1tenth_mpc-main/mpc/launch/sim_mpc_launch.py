4a26a2e8ac25dd5363c96ac034eeb50e72773973
qh65 <qh65@duke.edu>
2024-04-07 15:00:54 -0400

    Add sim_ws

---
+++ Code/sim_ws/src/f1tenth_mpc-main/mpc/launch/sim_mpc_launch.py
@@ -0,0 +1,30 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    share_directory = os.path.join(
+        get_package_share_directory('mpc'),
+        'waypoints', "")
+    return LaunchDescription([
+        Node(
+            package='mpc',
+            executable='mpc_node.py',
+            name='mpc_node',
+            parameters=[
+                {
+                'waypoints_path' : share_directory + "Hudson.csv"
+                }
+            ],
+            output='screen', # this is to see print statements
+        ),
+        Node(
+            package='pure_pursuit',
+            executable='pose_fake_pub_node',
+            name='pose_fake_pub_node',
+        )
+    ])
