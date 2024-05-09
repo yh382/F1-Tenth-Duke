6880adb9f01e9fe74aef601f03685c74e0aca247
Jim Huang <qh65@duke.edu>
2024-05-09 13:16:30 -0400

    upload f1tenth_ws

---
+++ Code/f1tenth-ws/build/range_lib/vendor/gflags/CMakeFiles/gflags_nothreads_static.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/f1tenth/f1tenth-ws/src/range_libc/vendor/gflags/src/gflags.cc" "/home/f1tenth/f1tenth-ws/build/range_lib/vendor/gflags/CMakeFiles/gflags_nothreads_static.dir/src/gflags.cc.o"
+  "/home/f1tenth/f1tenth-ws/src/range_libc/vendor/gflags/src/gflags_completions.cc" "/home/f1tenth/f1tenth-ws/build/range_lib/vendor/gflags/CMakeFiles/gflags_nothreads_static.dir/src/gflags_completions.cc.o"
+  "/home/f1tenth/f1tenth-ws/src/range_libc/vendor/gflags/src/gflags_reporting.cc" "/home/f1tenth/f1tenth-ws/build/range_lib/vendor/gflags/CMakeFiles/gflags_nothreads_static.dir/src/gflags_reporting.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BASEPATH=/home/f1tenth/f1tenth-ws/src/range_libc"
+  "GFLAGS_IS_A_DLL=0"
+  "NO_THREADS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "vendor/gflags/include"
+  "/home/f1tenth/f1tenth-ws/src/range_libc/vendor/gflags/src"
+  "vendor/gflags/include/gflags"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Code/f1tenth-ws/build/waypoint_python/build/lib/waypoint_python/waypoints_logger.py
@@ -0,0 +1,57 @@
+import rclpy
+import math
+from rclpy.node import Node
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Quaternion
+from scipy.spatial.transform import Rotation as R
+import numpy as np
+from numpy.linalg import norm
+import os
+from time import gmtime, strftime, time
+
+class WaypointsLogger(Node):
+    def __init__(self):
+        super().__init__('waypoints_logger')
+        self.subscription = self.create_subscription(Odometry, '/odom', self.save_waypoint, 10)
+        
+        
+        home = os.path.expanduser('~')
+        self.file = open(strftime(f'{home}/f1tenth-ws/src/waypoint_python/wp-%Y-%m-%d-%H-%M-%S.csv', gmtime()), 'w')
+        self.last_write_time=time()
+        self.write_interval = 0.05
+        self.get_logger().info('Saving waypoints...')
+
+    def save_waypoint(self, msg):
+        current_time = time()
+        
+        if current_time - self.last_write_time >= self.write_interval:
+            x = msg.pose.pose.position.x
+            y = msg.pose.pose.position.y
+            speed = norm(np.array([msg.twist.twist.linear.x, msg.twist.twist.linear.y]), 2)
+            q = [msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,msg.pose.pose.orientation.z,msg.pose.pose.orientation.w]
+            quat = R.from_quat(q)
+            euler = quat.as_euler("zxy",degrees = False)
+            yaw = euler[0] + 2*math.pi
+
+            
+            self.file.write(f'{x}, {y}, {speed}, {yaw}\n')
+            self.last_write_time = current_time
+
+
+
+
+    def shutdown(self):
+        self.file.close()
+        self.get_logger().info('Goodbye')
+
+def main(args=None):
+    rclpy.init(args=args)
+    waypoints_logger = WaypointsLogger()
+    rclpy.spin(waypoints_logger)
+
+    waypoints_logger.shutdown()
+    waypoints_logger.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
