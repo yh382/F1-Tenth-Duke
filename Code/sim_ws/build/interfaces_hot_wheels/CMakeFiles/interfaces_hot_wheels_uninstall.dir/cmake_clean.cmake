43f4045bc4bebea80ccf0ba7ba4bea9943a95254
Jim Huang <156489129+qh65@users.noreply.github.com>
2024-06-04 15:09:11 -0400

    Delete Code/sim_ws/build/interfaces_hot_wheels directory

--- Code/sim_ws/build/interfaces_hot_wheels/CMakeFiles/interfaces_hot_wheels_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/interfaces_hot_wheels_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/interfaces_hot_wheels_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Code/sim_ws/build/interfaces_hot_wheels/interfaces_hot_wheels__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/william/sim_ws/build/interfaces_hot_wheels/interfaces_hot_wheels__py
-# Build directory: /home/william/sim_ws/build/interfaces_hot_wheels/interfaces_hot_wheels__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
